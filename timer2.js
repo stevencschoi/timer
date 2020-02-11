71148957ee128c5b489f100aba4dc6a1e754c9d1
Steven Choi <stevencschoi87@gmail.com>
2020-02-11 20:00:11 +0000

    interactive timer that beeps on user input

---
+++ timer2.js
@@ -0,0 +1,24 @@
+// create timer from user input
+const stdin = process.stdin;
+stdin.setRawMode(true);
+stdin.setEncoding('utf8');
+
+stdin.on('data', (key) => {
+  // \u0003 maps to ctrl+c input
+  if (key === '\u0003') {
+    console.log('Thanks for using me, ciao!')
+    process.exit();
+  }
+
+  if (key === 'b') {
+  // beep right away
+  process.stdout.write('beep');
+  process.stdout.write('\x07');
+  } else if (key >= 1 && key <= 9) {
+    console.log(`Setting timer for ${key} seconds...`);
+    setTimeout(() => {
+      process.stdout.write('beep');
+      process.stdout.write('\x07');
+    }, key * 1000);
+  }; 
+});
