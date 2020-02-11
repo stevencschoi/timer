ae8b56843c7f9f54f5c7f3d2519110d0fd518267
Steven Choi <stevencschoi87@gmail.com>
2020-02-11 20:04:32 +0000

    ran eslint to update formatting

--- timer2.js
+++ timer2.js
@@ -6,19 +6,19 @@ stdin.setEncoding('utf8');
 stdin.on('data', (key) => {
   // \u0003 maps to ctrl+c input
   if (key === '\u0003') {
-    console.log('Thanks for using me, ciao!')
+    console.log('Thanks for using me, ciao!');
     process.exit();
   }
 
   if (key === 'b') {
   // beep right away
-  process.stdout.write('beep');
-  process.stdout.write('\x07');
+    process.stdout.write('beep');
+    process.stdout.write('\x07');
   } else if (key >= 1 && key <= 9) {
     console.log(`Setting timer for ${key} seconds...`);
     setTimeout(() => {
       process.stdout.write('beep');
       process.stdout.write('\x07');
     }, key * 1000);
-  }; 
+  }
 });
